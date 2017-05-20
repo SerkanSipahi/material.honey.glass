321a2b0aa0a9359f3a379d0ab9c455274dc701d4
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-05-20 17:37:51 +0200

    hack updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,15 @@
 let postcssNested = require('postcss-nested');
 let autoprefixer = require('autoprefixer');
 let cssnano = require('cssnano');
+let customSelector = require('postcss-custom-selectors');
 
 module.exports = {
     plugins: [
         postcssNested,
         autoprefixer({
             browsers: ["last 2 versions"]
-        })//,
-        //cssnano
+        }),
+        customSelector,
+        cssnano
     ],
 };
